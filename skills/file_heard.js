a73cc01817359e3b40018091a17e45cd506a88ed
Sameer Mehta <smehtaca@users.noreply.github.com>
2018-01-29 10:18:28 -0500

    Got Demographic model working

--- skills/file_heard.js
+++ skills/file_heard.js
@@ -11,14 +11,22 @@ module.exports = controller => {
     "direct_message,direct_mention",
     (bot, message) => {
       let url = message.text.substr(1).slice(0, -1);
-      console.log(url);
+
       app.models.predict("c0c0ac362b03416da06ab3fa36fb58e3", url).then(
         function(response) {
           let output = response.outputs[0].data.regions[0].data.face;
-          let age = output.age_apperance.concepts[0].name;
-          let gender = output.gender_apperance.concepts[0].name;
-          let multicultural = output.multicultural_apperance.concepts[0].name;
-          console.log(response);
+          let age = output.age_appearance.concepts[0].name || null;
+          let gender = output.gender_appearance.concepts[0].name || null;
+          let multicultural =
+            output.multicultural_appearance.concepts[0].name || null;
+          let result =
+            "You seem to be " +
+            age +
+            " years of age and of the " +
+            gender +
+            " gender. Additionally, you seem to be " +
+            multicultural;
+          bot.reply(message, result, message.team);
         },
         function(err) {
           console.log(err.status);
